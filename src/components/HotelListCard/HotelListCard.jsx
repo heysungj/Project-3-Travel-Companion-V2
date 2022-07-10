41c9b3f13736886c4550aac5552b7d1806c44d27
Mark Gerken <mgerken86@gmail.com>
2022-07-10 17:36:16 -0600

    Index Page styling

--- src/components/HotelListCard/HotelListCard.jsx
+++ src/components/HotelListCard/HotelListCard.jsx
@@ -13,31 +13,52 @@ export default function HotelListCard({
   const navigate = useNavigate();
 
   return (
-    <div>
-      <img src={hotel.max_photo_url} alt="" />
-      <h2>{hotel.hotel_name}</h2>
-      <h3>Review Score: {hotel.review_score}</h3>
-      <p>{hotel.is_free_cancellable ? "Free cancellation" : ""}</p>
-      <h2>
-        {hotel.currency_code}{" "}
-        {hotel.composite_price_breakdown.gross_amount_per_night.value.toFixed(
-          0
-        )}
-      </h2>
-      <button
-        onClick={() => {
-          navigate(
-            `/hotels/${hotel.hotel_id}?checkin=${checkIn}&checkout=${checkOut}&numberOfPerson=${numberOfPerson}`,
-            {
-              state: {
-                markers: { marker },
-              },
-            }
-          );
-        }}
-      >
-        Check Availability
-      </button>
+    <div className="favorite-place-container">
+      <div className="favPlaceLeftCol">
+
+        <h2>{hotel.hotel_name}</h2>
+        <h3>Review Score: {hotel.review_score}</h3>
+        <button
+          onClick={() => {
+            navigate(
+              `/hotels/${hotel.hotel_id}?checkin=${checkIn}&checkout=${checkOut}&numberOfPerson=${numberOfPerson}`,
+              {
+                state: {
+                  markers: { marker },
+                },
+              }
+            );
+          }}
+        >
+          Check Availability
+        </button>
+      </div>
+      <div className="favPlaceMidCol">
+        <h2>
+          {hotel.currency_code}{" "}$
+          {hotel.composite_price_breakdown.gross_amount_per_night.value.toFixed(
+            0
+          )}
+        </h2>
+        <p>{hotel.is_free_cancellable ? "Free cancellation" : ""}</p>
+      </div>
+      <div className="hotel-container">
+      <img 
+      className="list-page-img"
+      src={hotel.max_photo_url} 
+      alt="" 
+      />
+      <div className="img-absolute">
+        <h4>From {hotel.currency_code}{" "}$
+          {hotel.composite_price_breakdown.gross_amount_per_night.value.toFixed(
+            0
+          )}</h4>
+      </div>
+      </div>
+
+
+
+      
     </div>
   );
 }
