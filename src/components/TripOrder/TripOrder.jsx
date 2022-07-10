c35a53f1a1ddd3f4e4ea0845d0ce1f1c3669082f
Mark Gerken <mgerken86@gmail.com>
2022-07-10 15:03:09 -0600

    Order History search bar styled to Show Page search bar

--- src/components/TripOrder/TripOrder.jsx
+++ src/components/TripOrder/TripOrder.jsx
@@ -77,125 +77,124 @@ export default function TripOrder({ trip }) {
 
 
     return (
+
         <div className="favorite-place-container">
-            <div className="favPlaceLeftCol">
-                <h2>{trip.hotelName}</h2>
-                {/* if check-in date isn't in the past, you can delete/edit */}
-                {!disabled
-                    ? <div className='edit'>
-                        <button
-                            className='searchBtn'
-                            style={{backgroundColor:"#ff4d4d"}}
-                            onClick={() => {
-                                handleCancelBtn(trip._id)
-                                //This re-renders the component through useNavigate
-                                navigate(0)
-                            }}>
-                            Cancel This Trip
-                        </button>
-                        <button
-                            className='searchBtn'
-                            onClick={() => { setShowRooms(!showRooms) }}>
-                            Edit Your Reservation
-                        </button>
-                    </div>
-                    : <div className='no-edit'>You are unable to alter a reservation with a check-in date in the past</div>
-                }
-            </div>
-            <div className="favPlaceMidCol">
-                <h3>{trip.roomName}</h3>
-                <h4>Check-in: {trip.checkIn.slice(0, 10)} </h4>
-                <h4>Check-out: {trip.checkOut.slice(0, 10)}</h4>
-                <br />
-                <h4>Total Price: {trip.totalPrice}</h4>
-                <br />
-                <h4>Number of Guests: {trip.numberOfPeople}</h4>
-            </div>
-            <div className="hotel-container">
-                <h3 className="hotelH3">Your Accommodation</h3>
-                <img src={trip.hotelPhoto} alt="" />
-            </div>
-
-            <p></p>
-
-
-
-            {showRooms && <div className='edit-container'>
-                <h3>Choose Your Room at {trip.hotelName}</h3>
-                <div className="roomSearchBar">
-                    <div className='searchItem'>
-                        <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
-                        <input
-                            className='headerSearchInput'
-                            type="date"
-                            name="checkIn"
-                            value={data.checkIn}
-                            onChange={changeData}
-                            placeholder="Check-in Date"
-                            required
-                        />
-                    </div>
-                    <div className='searchItem'>
-                        <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
-                        <input
-                            className='headerSearchInput'
-                            type="date"
-                            name="checkOut"
-                            value={data.checkOut}
-                            onChange={changeData}
-                            placeholder="Checkout Date"
-                            required
-                        />
-                    </div>
-                    <div className='searchItem'>
-                        <FontAwesomeIcon icon={faPerson} className="headerIcon" />
-                        <input
-                            className='headerSearchInput'
-                            type="number"
-                            name="people"
-                            value={data.people}
-                            onChange={changeData}
-                            required
-                        />
-                    </div>
-                    <div className='searchItem'>
-                        <button
-                            className='headerBtn'
-                            onClick={() =>
-                                fetchAPI.getRoomDetails(
-                                    data.checkIn,
-                                    data.checkOut,
-                                    data.people,
-                                    trip.hotelId,
-                                    setRoomPhoto,
-                                    setRooms)
-                            }>
-                            Search
-                        </button>
-                    </div>
-                </div>
-                {rooms.length
-                    ?
-                    rooms.map((room, index) => {
-                        return (
-                            <div key={index}>
-                                <img
-                                    src={roomPhoto[room.room_id].photos[0].url_original}
-                                    alt=""
-                                />
-                                <h4>{room.name}</h4>
-                                <h4>Max Occupancy: {room.max_occupancy}</h4>
-                                <h4>Total Cost: $ {room.price_breakdown.gross_price}</h4>
+            {!showRooms ?
+                <>
+                    <div className="favPlaceLeftCol">
+                        <h2>{trip.hotelName}</h2>
+                        {/* if check-in date isn't in the past, you can delete/edit */}
+                        {!disabled
+                            ? <div className='edit'>
                                 <button
-                                    className='headerBtn'
-                                    onClick={() => { handleEdit(room) }}>
-                                    Change to This Room
+                                    className='searchBtn'
+                                    style={{ backgroundColor: "#ff4d4d" }}
+                                    onClick={() => {
+                                        handleCancelBtn(trip._id)
+                                        //This re-renders the component through useNavigate
+                                        navigate(0)
+                                    }}>
+                                    Cancel This Trip
+                                </button>
+                                <button
+                                    className='searchBtn'
+                                    onClick={() => { setShowRooms(!showRooms) }}>
+                                    Edit Your Reservation
                                 </button>
                             </div>
-                        );
-                    })
-                    : <p className='no-edit'>Sorry, no available rooms for those dates</p>}
-            </div>}
+                            : <div className='no-edit'>You are unable to alter a reservation with a check-in date in the past</div>
+                        }
+                    </div>
+                    <div className="favPlaceMidCol">
+                        <h3>{trip.roomName}</h3>
+                        <h4>Check-in: {trip.checkIn.slice(0, 10)} </h4>
+                        <h4>Check-out: {trip.checkOut.slice(0, 10)}</h4>
+                        <br />
+                        <h4>Total Price: {trip.totalPrice}</h4>
+                        <br />
+                        <h4>Number of Guests: {trip.numberOfPeople}</h4>
+                    </div>
+                    <div className="hotel-container">
+                        <h3 className="hotelH3">Your Accommodation</h3>
+                        <img src={trip.hotelPhoto} alt="" />
+                    </div>
+                </>
+                : <>
+                    <div className="favPlaceLeftCol">
+                    <div className="listSearch">
+                        <div>
+                        <label>Check In</label>
+                            <input
+                                className='SearchInput'
+                                type="date"
+                                name="checkIn"
+                                value={data.checkIn}
+                                onChange={changeData}
+                                placeholder="Check-in Date"
+                                required
+                            />
+                        </div>
+                        <div>
+                        <label>Check Out</label>
+                            <input
+                                className='SearchInput'
+                                type="date"
+                                name="checkOut"
+                                value={data.checkOut}
+                                onChange={changeData}
+                                placeholder="Checkout Date"
+                                required
+                            />
+                        </div>
+                        <div>
+                        <label>Number of Guests</label>
+                            <input
+                                className='SearchInput'
+                                type="number"
+                                name="people"
+                                value={data.people}
+                                onChange={changeData}
+                                required
+                            />
+                        </div>
+                            <button
+                                className='searchBtn'
+                                onClick={() =>
+                                    fetchAPI.getRoomDetails(
+                                        data.checkIn,
+                                        data.checkOut,
+                                        data.people,
+                                        trip.hotelId,
+                                        setRoomPhoto,
+                                        setRooms)
+                                }>
+                                Search
+                            </button>
+                        </div>
+                    </div>
+                    {rooms.length
+                        ?
+                        rooms.map((room, index) => {
+                            return (
+                                <div key={index}>
+                                    <img
+                                        src={roomPhoto[room.room_id].photos[0].url_original}
+                                        alt=""
+                                    />
+                                    <h4>{room.name}</h4>
+                                    <h4>Max Occupancy: {room.max_occupancy}</h4>
+                                    <h4>Total Cost: $ {room.price_breakdown.gross_price}</h4>
+                                    <button
+                                        className='headerBtn'
+                                        onClick={() => { handleEdit(room) }}>
+                                        Change to This Room
+                                    </button>
+                                </div>
+                            );
+                        })
+                        : <p className='no-edit'>Sorry, no available rooms for those dates</p>}
+                </>}
         </div>
+
     )
 }
